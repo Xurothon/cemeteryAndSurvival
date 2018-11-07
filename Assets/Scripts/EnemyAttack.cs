eec39f84a4ce84f93244fed0c6f169241634afcc
xurothon <kazachonakmaxim@gmail.com>
2018-11-07 17:56:43 +0300

    add enemy

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+	public float timeBetweenAttacks = 0.5f;
+	public int attackDamage = 10;
+	Animator animator;
+	GameObject player;
+	PlayerHealth playerHealth;
+	bool playerInRange;
+	float timer;
+
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		playerHealth = player.GetComponent<PlayerHealth>();
+		animator = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject == player){
+			playerInRange = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject == player){
+			playerInRange = false;
+		}
+	}
+	
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer >= timeBetweenAttacks && playerInRange){
+			Attack();
+		}
+		if(playerHealth.currentHealth <= 0){
+			animator.SetTrigger("PlayerDead");
+		}
+	}
+
+	void Attack(){
+		timer = 0f;
+		if(playerHealth.currentHealth > 0){
+			playerHealth.TakeDamage(attackDamage);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	public float flashSpeed = 5f;
+	public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+	Animator animator;
+	PlayerController playerMovement;
+	bool isDead;
+	bool damaged;
+
+	void Awake () {
+		animator = GetComponent<Animator>();
+		playerMovement = GetComponent<PlayerController>();
+		currentHealth = startingHealth;
+	}
+	
+	void Update () {
+		if(damaged){
+			damageImage.color = flashColor;
+		}
+		else{
+			damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
+	}
+
+	public void TakeDamage(int amount){
+		damaged = true;
+		currentHealth -= amount;
+		healthSlider.value = currentHealth;
+		if(currentHealth <= 0 && !isDead){
+			Death();
+		}
+	}
+
+	void Death(){
+		isDead = true;
+		animator.SetTrigger("Die");
+		playerMovement.enabled = false;
+	}
+}
