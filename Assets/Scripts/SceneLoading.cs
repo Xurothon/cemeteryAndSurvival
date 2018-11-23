ea2c04b728c63c3e8305696d91df233884f4f0c2
xurothon <kazachonakmaxim@gmail.com>
2018-11-23 10:21:02 +0300

    bug fixes

---
+++ Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoading : MonoBehaviour {
+
+	public string sceneName;
+	public Slider slider;
+	public Text progressText;
+
+	void Start () {
+		StartCoroutine(AsyncLoad());
+	}
+	
+	IEnumerator AsyncLoad(){
+		AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+		while(!operation.isDone){
+			float progress = operation.progress;
+			slider.value = progress;
+			progressText.text = string.Format("{0:0}%", progress*100);
+			yield return null; 
+		}
+	}
+}
